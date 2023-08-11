bbf0992f859bf391b5e6eb0071dd561981e6aec5
fred <exsample@gmail.com>
2023-08-11 23:29:11 +0300

    two

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ const namerOfFilms = prompt ("Сколько фильмов Вы уже посм
 
 const personalMovieDB = {
     count: namerOfFilms,
-    movies: [],
-    actors: [],
+    movies: {},
+    actors: {},
     genres: [],
-    privat: confirm("false")
+    privat: false
 };
-console.log(personalMovieDB);
+
 
 const answer1 = prompt("Один из последних просмотренных фильмов?");
 const answer2 = prompt("Насколько оцените его?");
+const answer3 = prompt("Один из последних просмотренных фильмов?");
+const answer4 = prompt("Насколько оцените его?");
+
+personalMovieDB.movies[answer1] = answer2;
+personalMovieDB.movies[answer3] = answer4;
+
+console.log(personalMovieDB);
